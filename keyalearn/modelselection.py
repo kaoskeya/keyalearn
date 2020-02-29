039610bb768684bf2810f82b565000a9d558a1c4
Karthikeyan S <k@kaos.agency>
2020-02-28 20:53:40 -0500

    no more printing, return both random state and test score

--- keyalearn/modelselection.py
+++ keyalearn/modelselection.py
@@ -4,17 +4,21 @@ def randomStateSelection(model, f, l, ts=0.2):
   max_score = 0
   random_state = 0
   for i in range(0, len(l)):
+    # generate random state
     X_train, X_test, y_train, y_test = train_test_split(f, l, test_size=ts, random_state=i)
     
     model.fit(X_train, y_train)
     train_score = model.score(X_train, y_train)
     test_score = model.score(X_test, y_test)
 
-    if(test_score > train_score) and test_score >= max_score:
+    if(test_score > train_score) and (test_score >= max_score):
       max_score = test_score
       random_state = i
+    
+    if max_score == 1:
+      break
   if random_state == 0:
-    print("Unbalanced model")
-    return 0
-  print("Use random state {} for the highest score of {}".format(random_state, max_score))
-  return random_state
+    # print("Unbalanced model")
+    return random_state, max_score
+  # print("Use random state {} for the highest score of {}".format(random_state, max_score))
+  return random_state, max_score
